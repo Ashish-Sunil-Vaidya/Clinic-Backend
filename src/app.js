29bad0a0039de9ae811de533400267737712c001
Ashish Vaidya <ashuvaidya2003@gmail.com>
2024-04-26 18:02:37 +0530

    Update CORS configuration and whitelist in app.js

--- src/app.js
+++ src/app.js
@@ -14,9 +14,16 @@ const app = express()
 import { openapi } from "../openapi.js"
 
 app.use(cors({
-    origin: "*",
+    origin: process.env.CORS_ORIGIN || "*",
     credentials: true
 }));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://main--sparkly-moonbeam-be963d.netlify.app');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
