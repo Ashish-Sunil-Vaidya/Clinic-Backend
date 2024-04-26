3f14a03da71a5d8096f9cd64b3aa200ad9941f94
Ashish Vaidya <ashuvaidya2003@gmail.com>
2024-04-26 17:35:16 +0530

    Refactor CORS configuration and add CORS whitelist in app.js

--- src/app.js
+++ src/app.js
@@ -11,9 +11,24 @@ import postmanToOpenApi from "postman-to-openapi";
 
 const app = express()
 
-import { openapi } from "../openapi.js" 
+import { openapi } from "../openapi.js"
 
-app.use(cors());
+
+const corsOptions = {
+    origin: function (origin, callback) {
+
+        const whitelist = [process.env.CORS_ORIGIN]
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,  // Allow cookies to be sent with requests
+};
+
+app.use(cors(corsOptions));
+
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
@@ -60,8 +75,8 @@ app.get('/generate-yml', async (req, res) => {
         console.log(err)
     }
 });
-app.get('/' , (req,res)=>{ 
-    res.send("Welcome to backend of clinic management system developed by --Syed Waseem(Code Surgery Squad)");  
- }) 
+app.get('/', (req, res) => {
+    res.send("Welcome to backend of clinic management system developed by --Syed Waseem(Code Surgery Squad)");
+})
 export { app }
 
