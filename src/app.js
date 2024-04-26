ab2e52c5df38491659bcf6a0f93cac07648fdcb2
Ashish Vaidya <ashuvaidya2003@gmail.com>
2024-04-26 18:05:21 +0530

    hfgh

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://main--sparkly-moonbeam-be963d.netlify.app');
+    res.header('Access-Control-Allow-Origin', 'https://main--sparkly-moonbeam-be963d.netlify.app/login');
     res.header('Access-Control-Allow-Credentials', 'true');
     next();
 });
