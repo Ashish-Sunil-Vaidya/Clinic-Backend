99a4fbd2a2f853d16bc186fefe3d2fdcfd19e945
Ashish Vaidya <ashuvaidya2003@gmail.com>
2024-04-26 17:44:21 +0530

    Refactor CORS configuration and update CORS whitelist in app.js

--- src/app.js
+++ src/app.js
@@ -13,22 +13,10 @@ const app = express()
 
 import { openapi } from "../openapi.js"
 
-
-const corsOptions = {
-    origin: function (origin, callback) {
-
-        const whitelist = [process.env.CORS_ORIGIN]
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true,  // Allow cookies to be sent with requests
-};
-
-app.use(cors(corsOptions));
-
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || "*",
+    credentials: true
+}));
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
